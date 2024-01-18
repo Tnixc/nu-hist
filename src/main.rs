096bba32b1906e452a10b173b4bad6381a2e5128
Tnixc <tnixxc@gmail.com>
2024-01-17 19:55:50 -0500

    Delete history entry with id 366

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use rusqlite::{Connection, Result};
-fn main() {
-  println!("Hello, world!");
+use rusqlite::{ Connection, Result };
+
+fn main() -> Result<()> {
+  // Connect to the database (create it if it doesn't exist)
+  let conn = Connection::open("history.sqlite3")?;
+  conn.execute(
+    "delete from history where id = 366;",
+    []
+  )?;
+  Ok(())
 }
