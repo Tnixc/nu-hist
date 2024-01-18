68ccb6719fbeb3a54eee2a9d637f3b92a9332c1a
Tnixc <tnixxc@gmail.com>
2024-01-18 13:10:34 -0500

    Commented out unused code

--- src/lib.rs
+++ src/lib.rs
@@ -146,29 +146,29 @@ fn year_to_unix(year: i32) -> (i64, i64) {
   return (start * 1000, end * 1000);
 }
 
-pub fn rand_string(len: usize, chars: &str) -> String {
-  let mut rng = rand::thread_rng();
-  let mut s = String::with_capacity(len);
-  for _ in 0..len {
-    let idx = rng.gen_range(0..chars.len());
-    s.push(chars.chars().nth(idx).unwrap());
-  }
-  return s;
-}
+// pub fn rand_string(len: usize, chars: &str) -> String {
+//   let mut rng = rand::thread_rng();
+//   let mut s = String::with_capacity(len);
+//   for _ in 0..len {
+//     let idx = rng.gen_range(0..chars.len());
+//     s.push(chars.chars().nth(idx).unwrap());
+//   }
+//   return s;
+// }
 
-pub fn fill_data(conn: &Connection) -> Result<()> {
-  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  for _ in 0..100_000 {
-    let command = rand_string(3, chars);
-    let hostname = rand_string(4, chars);
-    let time: String = rand
-      ::thread_rng()
-      .gen_range(1671512400..1704949200)
-      .to_string();
-    conn.execute(
-      "insert into history (command_line, start_timestamp, hostname) values (?1, ?2, ?3)",
-      [&command, &time, &hostname]
-    )?;
-  }
-  return Ok(());
-} // Haha this is just for side effects
+// pub fn fill_data(conn: &Connection) -> Result<()> {
+//   let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+//   for _ in 0..100_000 {
+//     let command = rand_string(3, chars);
+//     let hostname = rand_string(4, chars);
+//     let time: String = rand
+//       ::thread_rng()
+//       .gen_range(1671512400..1704949200)
+//       .to_string();
+//     conn.execute(
+//       "insert into history (command_line, start_timestamp, hostname) values (?1, ?2, ?3)",
+//       [&command, &time, &hostname]
+//     )?;
+//   }
+//   return Ok(());
+// } // Haha this is just for side effects
