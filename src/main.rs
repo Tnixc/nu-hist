37b380490510ba3c017d0e987e715bb8a7bcad24
Tnixc <tnixxc@gmail.com>
2024-01-17 20:04:03 -0500

    Refactor database query and add character set

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ use rusqlite::{ Connection, Result };
 fn main() -> Result<()> {
   // Connect to the database (create it if it doesn't exist)
   let conn = Connection::open("history.sqlite3")?;
-  conn.execute(
-    "delete from history where id = 366;",
-    []
-  )?;
+  // let mut content = conn.prepare("select * from history")?;
+  // let mut rows = content.query([])?;
+  // while let Some(row) = rows.next()? {
+  //   let time: i64 = row.get(2)?;
+  //   let command: String = row.get(1)?;
+  //   println!("{}: {:?}", time, command)
+  // }
+  let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  
   Ok(())
 }
