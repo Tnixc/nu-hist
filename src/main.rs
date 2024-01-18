741b4cc701c390a3c2ff80f75d1f0cbd8ec499c6
Tnixc <tnixxc@gmail.com>
2024-01-17 20:16:08 -0500

    Refactor main.rs: Remove commented code and add fill_data function

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use rusqlite::{ Connection, Result };
 use rand::Rng;
 
 fn main() -> Result<()> {
-  // Connect to the database (create it if it doesn't exist)
   let conn = Connection::open("history.sqlite3")?;
   // let mut content = conn.prepare("select * from history")?;
   // let mut rows = content.query([])?;
@@ -11,19 +10,7 @@ fn main() -> Result<()> {
   //   let command: String = row.get(1)?;
   //   println!("{}: {:?}", time, command)
   // }
-  let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  for _ in 0..100 {
-    let command = rand_string(3, chars);
-    let hostname = rand_string(4, chars);
-    let time: String = rand
-      ::thread_rng()
-      .gen_range(0..1000000000)
-      .to_string();
-    conn.execute(
-      "insert into history (command_line, time, hostname) values (?1, ?2, ?3)",
-      &[&command, &time, &hostname]
-    )?;
-  }
+
   Ok(())
 }
 
@@ -35,4 +22,21 @@ fn rand_string(len: usize, chars: &str) -> String {
     s.push(chars.chars().nth(idx).unwrap());
   }
   return s;
+}
+
+fn fill_data(conn: &Connection) -> Result<()> {
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  for _ in 0..100_000 {
+    let command = rand_string(3, chars);
+    let hostname = rand_string(4, chars);
+    let time: String = rand
+      ::thread_rng()
+      .gen_range(1671512400..1704949200)
+      .to_string();
+    conn.execute(
+      "insert into history (command_line, start_timestamp, hostname) values (?1, ?2, ?3)",
+      &[&command, &time, &hostname]
+    )?;
+  }
+  Ok(())
 }
